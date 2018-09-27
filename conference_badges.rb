49b6701073c8ddb9e6cb6b019b7556dadadc4bef
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-27 09:08:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,6 @@ end
 
 def assign_rooms(attendees)
   attendees.collect do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index += 1}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
