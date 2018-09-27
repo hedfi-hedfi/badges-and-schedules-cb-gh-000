08ca84c4380e183e135b89354c13b49b99065ded
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-27 09:01:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect { |attendee| badge_maker(attendee) }
+end
+
+def assign_rooms(attendees)
+  attendees.each
+end
