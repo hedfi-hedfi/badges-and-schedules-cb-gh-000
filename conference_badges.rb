fedab044158ecc14c2a1f3ed09488ea15e9dc824
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-27 09:09:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.collect_with_index do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  index = 0
+  attendees.collect do |attendee|
+    "Hello, #{attendee}! You'll be assigned to room #{index +=1}!"
+    
   end
 end
