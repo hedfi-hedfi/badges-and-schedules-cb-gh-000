14d419911d92c37fdcfa78e7c5001b0bdb9bff4d
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-27 09:14:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  #badge_maker(attendees).each do { |attendee| puts attendee }
+  batch_badge_creator(attendees).each do { |attendee| puts attendee }
 end
